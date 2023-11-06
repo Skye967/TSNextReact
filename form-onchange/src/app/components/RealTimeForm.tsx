861728d14b1a249069574ff72bab06b9d0502d62
AKAbigdaddy <akabigdaddy@skyes-MBP.home>
2023-11-06 08:34:53 -0700

    form-onchange

---
+++ form-onchange/src/app/components/RealTimeForm.tsx
@@ -0,0 +1,73 @@
+import React, {useState} from 'react'
+
+
+const RealTimeForm = () => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+    
+    return (
+      <div>
+        <div>
+          <form>
+            <div>
+              <label>First Name</label>
+              <input
+                type="text"
+                onChange={(e) => setFirstName(e.target.value)}
+              ></input>
+            </div>
+            <div>
+              <label>Last Name</label>
+              <input
+                type="text"
+                onChange={(e) => setLastName(e.target.value)}
+              ></input>
+            </div>
+            <div>
+              <label>Email</label>
+              <input
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+              ></input>
+            </div>
+            <div>
+              <label>Password</label>
+              <input
+                type="text"
+                onChange={(e) => setPassword(e.target.value)}
+              ></input>
+            </div>
+            <div>
+              <label>Confirm Password</label>
+              <input
+                type="text"
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              ></input>
+            </div>
+          </form>
+        </div>
+        <div>
+          <p>
+            First Name: <span>{firstName}</span>
+          </p>
+          <p>
+            Last Name: <span>{lastName}</span>
+          </p>
+          <p>
+            Email: <span>{email}</span>
+          </p>
+          <p>
+            Password: <span>{password}</span>
+          </p>
+          <p>
+            Confirm Password: <span>{confirmPassword}</span>
+          </p>
+        </div>
+      </div>
+    );
+}
+
+export default RealTimeForm
---
+++ form-onchange/src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import RealTimeForm from "./components/RealTimeForm"
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Form</h1>
+      <RealTimeForm/>
+    </main>
+  )
+}
